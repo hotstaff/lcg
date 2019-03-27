3bc9ad375130c2c3c784d82b6ecbfc375874becb
Hideto Manjo <enjinit@gmail.com>
2019-03-27 09:32:21 +0900

    [update] Add test script. 	modified:   src/Makefile.am 	new file:   test/encode_test.sh 	new file:   test/sample.bin

---
+++ test/encode.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "---ENCODE/DECODE TEST---"
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+INPUT=$SCRIPT_DIR/sample.bin
+OUTPUT=$SCRIPT_DIR/output.bin
+INPUT_R=$SCRIPT_DIR/sample_r.bin
+
+rm OUTPUT
+rm INPUT_R
+../src/lcg $INPUT $OUTPUT &> /dev/null
+../src/lcg -d $OUTPUT $INPUT_R &> /dev/null
+
+cmp -lb $INPUT $INPUT_R
+if [ "$?" -eq 0 ]; then
+	echo "Test success."
+	exit 0
+fi
+
+echo "Test fail."
+exit 1
+
+
---
+++ test/encrypt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "---ENCRYPT/DECRYPT TEST---"
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+INPUT=$SCRIPT_DIR/sample.bin
+OUTPUT=$SCRIPT_DIR/output_encrypted.bin
+INPUT_R=$SCRIPT_DIR/sample_decrypted.bin
+KEY=$SCRIPT_DIR/output_encrypted.key
+
+rm $OUTPUT
+rm $INPUT_R
+rm $KEY
+
+../src/lcg $INPUT $OUTPUT -k $KEY &> /dev/null
+../src/lcg -d $OUTPUT $INPUT_R -k $KEY &> /dev/null
+
+cmp -lb $INPUT $INPUT_R
+if [ "$?" -eq 0 ]; then
+	echo "Test success."
+	exit 0
+fi
+
+echo "Test fail."
+exit 1
+
+
